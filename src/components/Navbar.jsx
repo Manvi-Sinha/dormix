2974656dcdd44084afcc677a290b57f426c65ad2
Manvi Sinha <manvisinha766@gmail.com>
2026-07-15 12:22:24 +0530

    Initial React project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Dormix</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <h1>Welcome to Dormix</h1>
+      <p>Smart Hostel/PG Management Platform</p>
+    </>
+  );
+}
+
+export default Home;
