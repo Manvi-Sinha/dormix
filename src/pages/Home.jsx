fda7a5b8c7bac44f3af372827af28b78a115955f
Manvi Sinha <manvisinha766@gmail.com>
2026-07-15 12:29:31 +0530

    Install Tailwind CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 function Home() {
   return (
-    <>
-      <h1>Welcome to Dormix</h1>
-      <p>Smart Hostel/PG Management Platform</p>
-    </>
+    <div className="min-h-screen bg-slate-950 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold text-blue-500">
+        Dormix
+      </h1>
+
+      <p className="mt-4 text-xl text-gray-300">
+        Smart Hostel Management Platform
+      </p>
+
+      <button className="mt-8 bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg">
+        Explore Now
+      </button>
+    </div>
   );
 }
 
